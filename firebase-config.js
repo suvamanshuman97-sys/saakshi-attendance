bc9775f6230397317be547dba7482078fe0f20bf
suvamanshuman97-sys <suvam.anshuman97@gmail.com>
2026-07-30 00:34:08 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-ToxNwUCaRKLJYgQ7yVRhYgRmOfS85Z0",
+  authDomain: "saakshi-fe5a0.firebaseapp.com",
+  databaseURL: "https://saakshi-fe5a0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "saakshi-fe5a0",
+  storageBucket: "saakshi-fe5a0.firebasestorage.app",
+  messagingSenderId: "588714154873",
+  appId: "1:588714154873:web:f92a4a644d3ce671b12da8"
+};
+
+firebase.initializeApp(firebaseConfig);
